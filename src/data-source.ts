c2d1c011b043bf62c2a504b823ec47bad2b85f01
vishalp-65 <vishalpanchal1828vp@gmail.com>
2024-09-26 17:08:17 +0530

    Fix production issue(entities not intilized)

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,32 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import "reflect-metadata";
 import { config } from "./config/server_config";
 
 const AppDataSource = new DataSource({
     type: "mysql",
     url: config.DB_URI,
     synchronize: true,
-    // logging: true,
-    entities: ["src/entities/**/*.ts"],
-    migrations: [],
+    logging: true,
+    entities: [
+        process.env.NODE_ENV === "production"
+            ? "dist/entities/**/*.js" // Use JS files in production
+            : "src/entities/**/*.ts", // Use TS files in development
+    ],
+    migrations: [
+        process.env.NODE_ENV === "production"
+            ? "dist/migrations/**/*.js" // Use JS files in production
+            : "src/migrations/**/*.ts", // Use TS files in development
+    ],
     subscribers: [],
 });
 
 AppDataSource.initialize()
-    .then(() => console.log("DB connected"))
-    .catch((err) => console.log("Error connecting DB", err));
+    .then(() => {
+        console.log("Data Source has been initialized!");
+    })
+    .catch((error) => {
+        console.error("Error during Data Source initialization:", error);
+    });
 
 export default AppDataSource;
